45f6367a0e97a4bef110c5e3115a82c06188a799
Lan <lan@LandeMacBook-Air.local>
2024-07-20 12:21:47 +0800

    first commit

---
+++ BlueTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueTech
+//
+//  Created by Lan on 2024/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BlueTech/Pods-BlueTech-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlueTech : NSObject
+@end
+@implementation PodsDummy_Pods_BlueTech
+@end
